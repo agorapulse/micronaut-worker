fb3cd6536411f7aa08a6863de3bf1ffb90bb2b3e
Vladimir Orany <vladimir@orany.cz>
2021-10-13 12:22:45 +0200

    [branch ch45837] Jobs endpoint

---
+++ libs/micronaut-worker/src/test/groovy/com/agorapulse/worker/management/JobsEndpointSpec.groovy
@@ -0,0 +1,63 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2021 Agorapulse.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.agorapulse.worker.management
+
+import com.agorapulse.gru.Gru
+import com.agorapulse.worker.annotation.InitialDelay
+import groovy.transform.CompileStatic
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.AutoCleanup
+import spock.lang.Specification
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@MicronautTest(environments = JOBS_ENDPOINT_SPEC_ENVIRONMENT)
+@Property(name = 'worker.jobs.sample-job.enabled', value = 'true')
+class JobsEndpointSpec extends Specification {
+
+    public static final String JOBS_ENDPOINT_SPEC_ENVIRONMENT = 'jobs-endpoint-spec-environment'
+
+    @AutoCleanup @Inject Gru gru
+
+    void 'render job'() {
+        expect:
+            gru.test {
+                get '/jobs'
+                expect {
+                    json 'jobs.json'
+                }
+            }
+    }
+
+}
+
+@CompileStatic
+@Singleton
+@SuppressWarnings('EmptyMethod')
+@Requires(env = JobsEndpointSpec.JOBS_ENDPOINT_SPEC_ENVIRONMENT)
+class SampleJob {
+
+    @InitialDelay('10m')
+    void run() {
+        // some code here
+    }
+
+}
