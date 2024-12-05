7e679413dc4d75203867d6f0d79947bb4815f981
musketyr <vladimir@orany.cz>
2024-12-05 10:42:14 +0100

    returned deprecated method

--- libs/micronaut-worker/src/main/java/com/agorapulse/worker/queue/JobQueues.java
+++ libs/micronaut-worker/src/main/java/com/agorapulse/worker/queue/JobQueues.java
@@ -22,8 +22,25 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.function.Consumer;
 
 public interface JobQueues {
+
+    /**
+     * Reads messages from the queue and processes them with the given action.
+     * @param queueName the name of the queue
+     * @param maxNumberOfMessages the maximal number of messages to read
+     * @param waitTime the maximal time to wait for the messages
+     * @param argument the argument type
+     * @param action the action to process the message
+     * @param <T> the type of the message
+     * @deprecated Use {@link #readMessages(String, int, Duration, Argument)} instead
+     */
+    @Deprecated(forRemoval = true)
+    default <T> void readMessages(String queueName, int maxNumberOfMessages, Duration waitTime, Argument<T> argument, Consumer<T> action) {
+        Flux.from(readMessages(queueName, maxNumberOfMessages, waitTime, argument)).map(QueueMessage::getMessage).subscribe(action);
+    }
+
 
     /**
      * Reads messages from the queue and processes them with the given action.
